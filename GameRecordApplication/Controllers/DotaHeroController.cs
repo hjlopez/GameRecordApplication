c12b4b0e018c88a99743972f535fb77ff1950abf
hjlopez <49752106+hjlopez@users.noreply.github.com>
2019-11-11 21:05:06 +0800

    - started making a dummy controller for External API

---
+++ GameRecordApplication/Controllers/DotaHeroController.cs
@@ -0,0 +1,45 @@
+﻿using GameRecordApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameRecordApplication.Controllers
+{
+    public class DotaHeroController : Controller
+    {
+        public ActionResult Index()
+        {
+            IEnumerable<DotaHero> students = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.opendota.com/api/heroes");
+                //HTTP GET
+                var responseTask = client.GetAsync("heroes");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<DotaHero>>();
+                    readTask.Wait();
+
+                    students = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    students = Enumerable.Empty<DotaHero>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            
+            return View(students.ToList());
+        }
+    }
+}
---
+++ GameRecordApplication/Models/DotaHero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameRecordApplication.Models
+{
+    public class DotaHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Localized_name { get; set; }
+        public string Primary_attr { get; set; }
+        public string Attack_type { get; set; }
+        public List<string> Roles { get; set; }
+        public int Legs { get; set; }
+    }
+}
